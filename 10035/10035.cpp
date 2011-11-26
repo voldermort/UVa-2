c7ef9e66f6a813b18ea412e7e06ff123532c41bd
Alex <alx.velazquez@gmail.com>
2011-11-25 20:35:38 -0500

    UVa PotD 3

---
+++ 10035/10035.cpp
@@ -0,0 +1,20 @@
+/* Alex Velazquez
+ * Start: Fri Nov 25 20:32:26 EST 2011
+ * End  :  ?
+*/
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <sstream>
+#include <cmath>
+#include <cstdio>
+#include <deque>
+
+using namespace std;
+
+int main ()
+{
+
+}
